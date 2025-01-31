0d3cbe64afec342b1307ea3b41e9daaeafee88d4
aditya-2000-pundir <pundhir2000aditya@Gmail.com>
2025-01-31 11:00:47 +0530

    braj.mohan

--- harvendra.py
+++ harvendra.py
@@ -1,5 +1,4 @@
-print("\nFor Loop with Else:")
-for num in range(3):  
-    print(f"Number: {num}")  
-else:  
-    print("Loop completed without break.")
+count = 1
+while count <= 5:
+    print("Count:", count)
+    count += 1
