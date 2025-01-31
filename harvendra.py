832a46ce70fa3a832538629b7e4776655218a588
aditya-2000-pundir <pundhir2000aditya@Gmail.com>
2025-01-31 11:08:27 +0530

    DJ

--- harvendra.py
+++ harvendra.py
@@ -2,4 +2,18 @@ count = 1
 while count <= 10:
     print("Count:", count)
     count += 1
-    
+n = 5
+sum = 0
+i = 1
+while i <= n:
+    sum += i
+    i += 1
+print("Sum of numbers from 1 to", n, "is", sum)
+n = 3
+while n > 0:
+    print("Countdown:", n)
+    n -= 1
+else:
+    print("Done!")
+
+   
